bbaa7caf2256df950f94b94ea7bf79a2df8699c7
zxc-steve <63087711+zxc-steve@users.noreply.github.com>
2025-11-07 15:47:57 +0800

    Add overloaded template function f1 to A.cpp

--- A.cpp
+++ A.cpp
@@ -6,8 +6,16 @@ Ret f(Tp& a){cout<<"Tp& "<<a<<endl;}
 Ret f(const Tp& a){cout<<"const Tp& "<<a<<endl;}
 Ret f(Tp&& a){cout<<"Tp&& "<<a<<endl;}
 Ret f(const Tp&& a){cout<<"const Tp&& "<<a<<endl;}
-
+template <typename T> Ret f1(const T& a){f(a);cout<<" ----const T&"<<endl;}
+template <typename T> Ret f1(T& a){f(a);cout<<" ----T&"<<endl;}
+template <typename T> Ret f1( T&& a){f(a); cout<<" ----T&&"<<endl;}
 int main() {
-    
+    int i=0,&r1=i;
+    const int j=1,&r2=j;
+   // f(i);f(j);f(2);f(std::move(i));
+    f1(i);f1(j);
+    f1(2+r2);
+    f1(std::move(i));
     return 9;
 }
+
