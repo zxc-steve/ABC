8820ff82a215cfcb65783f05aaca2613a6efc4a6
zxc-steve <63087711+zxc-steve@users.noreply.github.com>
2025-11-11 15:29:15 +0000

    solve conflict

--- templateOverload.cpp
+++ templateOverload.cpp
@@ -23,8 +23,7 @@ template <typename T> string debug_rep(const T &t) {
 ostringstream ret; ret << t;return ret.str();}
 template <typename T> string debug_rep(T *p){
    ostringstream ret;ret << "pointer: " << p;
-    //if (p) ret << " " << debug_rep(*p);
-    //else ret << " null pointer";
+
     ret<<' '<<(p?debug_rep(*p):"null pointer");
     return ret.str();
 }
