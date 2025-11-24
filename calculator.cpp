1e513338452914739e248482a0a74683fb5f0ba8
zxc-steve <63087711+zxc-steve@users.noreply.github.com>
2025-11-24 15:11:56 +0000

    finished

--- calculator.cpp
+++ calculator.cpp
@@ -1,17 +1,32 @@
 //#include "PPP.h"
 #include <iostream>
 #include <stdexcept>
+#include <vector>
+//#include <string>
+using std::vector;
 using namespace std;
-[[noreturn]] inline void error(const std::string& s)	// error() simply disguises throws
-	{	throw std::runtime_error(s);	}
-enum:char { PRINT = ';', QUIT = 'q', NUMBER = '8',PROMPT='>',RESULT='=' }; // token kinds  
-class Token {
+[[noreturn]] inline void error(const std::string& s,const std::string& s1="")	
+    // error() simply disguises throws
+	{	throw std::runtime_error(s+s1);	}
+enum:char { PRINT = ';', QUIT = 'q', NUMBER = '8',PROMPT='>',RESULT='=',
+    LET='L',NAME='a' }; // token kinds
+const string declkey = "let"; // declaration keyword
+class Token
+{
 public:
     char kind;
     double value;
-    Token(char k) :kind{ k }, value{ 0.0 } {}
-    Token(char k, double v) :kind{ k }, value{ v } {}
+    string name;
+    Token() : kind{0} {}
+    Token(char ch) : kind{ch} {}
+    Token(char ch, double val) : kind{ch}, value{val} {}
+    Token(char ch, string n) : kind{ch}, name{n} {}
+    // default constructor 
+    // initialize kind with ch 
+    // initialize kind and value 
+    // initialize kind and name
 };
+;
 class Token_stream {
 public:
     Token get();
@@ -41,7 +56,7 @@ Token Token_stream::get()
     switch (ch) {
     case PRINT: // for "print"
     case QUIT: // for "quit"
-    case '(': case ')': 
+    case '(': case ')': case '=' :
     case '+': case '-': case '*': case '/': case '%':
         return Token{ ch };
     case '.':
@@ -54,105 +69,222 @@ Token Token_stream::get()
         return Token{ NUMBER ,val }; 
     }
     default:
-        error("Bad token");
+        if (isalpha(ch))
+        {
+            string s;
+            s += ch;
+            while (cin.get(ch) && (isalpha(ch) || isdigit(ch)))
+                s += ch;
+            cin.putback(ch);
+            if (s == declkey)
+                return Token{LET}; // declaration keyword return Token{name,s};
+            return Token{NAME,s};
+        }
+        error("Bad token",string(1,ch));
     }
+}
+void Token_stream::ignore(char c) // c represents the kind of Token
+{
+    if (full && c == buffer.kind)
+    { // ﬁrst look in buffer
+        full = false;
+        return;
+    }
+    full = false;
+    // now search input:
+    char ch = 0;
+    while (cin >> ch)
+        if (ch == c) return;
+}
+struct Variable { 
+    string name;
+    double value; 
+};
+
+vector<Variable> var_table;
+double get_value(string s) // return the value of the Variable named s
+{
+    for (const Variable &v : var_table)
+        if (v.name == s)    return v.value;
+    error("trying to read undeﬁned variable ", s);
+}
+void set_value(string s, double d) // set the Variable named s to d
+{
+    for (Variable &v : var_table)
+        if (v.name == s)
+        {
+            v.value = d;
+            return;
+        }
+    error("trying to write undeﬁned variable ", s);
+}
+bool is_declared(string var) // is var already in var_table?
+{
+    for (const Variable &v : var_table)
+        if (v.name == var)      return true;
+    return false;
+}
+
+double deﬁne_name(string var, double val) // add {var,val} to var_table
+{
+    if (is_declared(var))
+        error(var, " declared twice");
+    var_table.push_back(Variable{var, val});
+    return val;
 }
 
 Token_stream ts;
 double expression();
 
-double primary()
+double declaration() // assume we have seen "let’’ // handle: name = expression // declare a variable called "name’’ with the initial value "expression’’
 {
     Token t = ts.get();
-    switch (t.kind) {
-    case '(':
-    {
-        double d = expression();
-        t = ts.get();
-        if (t.kind != ')')
-            error("')' expected");
-        return d;
-    }
-    case NUMBER:           // we use ’8’ to represent a number
-        return t.value; // return the number’s value
-    case '-':   return - primary();
-    case '+':   return   primary();
-    default:
-        error("primary expected");
-    }
-}
-double term()
-{
-    double left = primary();
-    Token t = ts.get();
-    while (true) {
-        switch (t.kind) {
-        case '*':
-            left *= primary();
-            t = ts.get();
-            break;
-        case '/':
-        {
-            double d = primary();
-            if (d == 0)
-                error("divide by zero");
-            left /= d;
-            t = ts.get();
-            break;
-        }
-        case '%':
-        {
-            double d = primary();
-            if (d == 0)  error("%:divide by zero");
-            left = left-d*int(left/d);
-            t = ts.get();
-            break;
-        }
-        default:
-            ts.putback(t);
-            return left;
-        }
-    }
-}
-double expression()
-{
-    double left = term(); 
-    Token t = ts.get(); 
-    while (true) {
-        switch (t.kind) {
-        case '+':
-            left += term();
-            t = ts.get();
-            break;
-        case '-':
-            left -= term(); 
-            t = ts.get();
-            break;
-        default:
-            ts.putback(t);
-            return left;
-        }
-    }
-}
-void calculate(){
-    while (cin) {
-        cout << PROMPT;
-        Token t = ts.get();
-        while (t.kind == PRINT)   t = ts.get();// eat ’;’
+    if (t.kind != NAME)
+        error("name expected in declaration");
 
-        if (t.kind == QUIT)  return;
-        ts.putback(t);
-        cout << RESULT << expression() << '\n';
-    }   
+    Token t2 = ts.get();
+    if (t2.kind != '=')
+        error("= missing in declaration of ", t.name);
+        double d = expression(); 
+        deﬁne_name(t.name,d); 
+        return d;
 }
-int main()
-{   try
+double primary()
     {
-        calculate();
-        return 0;
+        Token t = ts.get();
+        switch (t.kind)
+        {
+        case '(':
+        {
+            double d = expression();
+            t = ts.get();
+            if (t.kind != ')')
+                error("')' expected");
+            return d;
+        }
+        case NUMBER:        // we use ’8’ to represent a number
+            return t.value; // return the number’s value
+        case '-':
+            return -primary();
+        case '+':
+            return primary();
+        case NAME:
+            return get_value(t.name);
+        default:
+            error("primary expected");
+        }
     }
-    catch (exception &e)
-    {   cerr << e.what() << '\n';   return 1;}
-    catch (...)
-    {    cerr << "exception \n";    return 2;}
-}
+    double term()
+    {
+        double left = primary();
+        Token t = ts.get();
+        while (true)
+        {
+            switch (t.kind)
+            {
+            case '*':
+                left *= primary();
+                t = ts.get();
+                break;
+            case '/':
+            {
+                double d = primary();
+                if (d == 0)
+                    error("divide by zero");
+                left /= d;
+                t = ts.get();
+                break;
+            }
+            case '%':
+            {
+                double d = primary();
+                if (d == 0)
+                    error("%:divide by zero");
+                left = left - d * int(left / d);
+                t = ts.get();
+                break;
+            }
+            default:
+                ts.putback(t);
+                return left;
+            }
+        }
+    }
+    double expression()
+    {
+        double left = term();
+        Token t = ts.get();
+        while (true)
+        {
+            switch (t.kind)
+            {
+            case '+':
+                left += term();
+                t = ts.get();
+                break;
+            case '-':
+                left -= term();
+                t = ts.get();
+                break;
+            default:
+                ts.putback(t);
+                return left;
+            }
+        }
+    }
+    double statement()
+    {
+        Token t = ts.get();
+        switch (t.kind)
+        {
+        case LET:
+            return declaration();
+        default:
+            ts.putback(t);
+            return expression();
+        }
+    }
+    void clean_up_mess() { ts.ignore(PRINT); }
+    void calculate()
+    {
+        while (cin)
+        {
+            try
+            {
+                cout << PROMPT;
+                Token t = ts.get();
+                while (t.kind == PRINT)
+                    t = ts.get(); // eat ’;’
+
+                if (t.kind == QUIT)
+                    return;
+                ts.putback(t);
+                cout << RESULT << statement() << '\n';
+            }
+            catch (exception &e)
+            {
+                cerr << e.what() << '\n';
+                clean_up_mess();
+            }
+        }
+    }
+    int main()
+    {
+        try
+        {
+            deﬁne_name("pi",3.1415926535);
+            deﬁne_name("e",2.7182818284);
+            calculate();
+            return 0;
+        }
+        catch (exception &e)
+        {
+            cerr << e.what() << '\n';
+            return 1;
+        }
+        catch (...)
+        {
+            cerr << "exception \n";
+            return 2;
+        }
+    }
