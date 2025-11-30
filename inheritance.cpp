0edd640ccf30a9fe25bf06068668ff6e1e957aab
zxc-steve <63087711+zxc-steve@users.noreply.github.com>
2025-11-30 09:29:52 +0000

    finshed

--- inheritance.cpp
+++ inheritance.cpp
@@ -2,7 +2,7 @@
 #include <iostream> 
 #include <vector>
 using namespace std; 
-class Base{
+class Base{ //Class with Pure Virtuals Are Abstract Base Class
     public:
     virtual void print()=0;
 };   
@@ -12,6 +12,7 @@ class X: public Base{
     string s;
     void print() override {cout << "X: " << x << ", " << s << endl;}
     X()=default;
+    //X(): x(0), s("") {}
     X(int x, string s): x(x), s(s) {}   
 };
 class Y: public X{
@@ -19,7 +20,8 @@ class Y: public X{
     int y;
     string s;
     void print() override {cout << "Y: " << y << ", " << s << endl;}
-    Y(int y, string s): y(y), s(s) {}  
+    Y(int y, string s): y(y), s(s) {} 
+    Y():X(0,""), y(0), s("") {} ; 
 };
 class Z: public Y{
     public:
@@ -27,17 +29,25 @@ class Z: public Y{
     string s;
     void print() override { cout << "Z: " << z << ", " << s << endl;}   
     Z(int z, string s): Y(z, s), z(z), s(s) {}  
+    Z():Z(1,"default") {} ;
 };
 class Container{
     public:
     vector<Base*> contents; 
-    void add(Base* b) {contents.push_back(b);
-    }
+    void add(Base* b) {contents.push_back(b);}
 };
+void f(char s[]){
+    while(*s!=0){
+    cout << "f(char*): " << *s++ << endl;
+    }
+}
 int main() {
+    
+    //f((char*)"Hello World");
     X x{10, string("Hello from X")};
     Y y{20, "Hello from Y"};
-    Z z(30, "Hello from Z");    
+    Z z(30, "Hello from Z");
+    Z a;    
     Base* ptr = &x;ptr->print();
     ptr = &y;ptr->print();
     ptr = &z;ptr->print();
@@ -54,4 +64,6 @@ int main() {
 3.Add virtual function with the same name and same signature
 4.Add operator+() overload, return lvalue reference
 5.Add operator+() overload, return by value
+6. Add virtual destructor to each class
+
 */
