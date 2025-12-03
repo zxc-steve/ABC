4210a9fe6f0e3d90b942b0e9d5dd716d3af8f5af
zxc-steve <63087711+zxc-steve@users.noreply.github.com>
2025-12-03 09:04:48 +0000

    debug()

--- My.h
+++ My.h
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-#define debug(x) cout<<#x<<"="<<x<<endl;
+#define debug(x) cout<<#x<<"="<<(x)<<endl;
 template<typename T>
 ostream& operator<<(ostream& os, vector<T>& obj) { 
     for (auto& x : obj) os << x << " "; return os; }
--- iterator.cpp
+++ iterator.cpp
@@ -5,6 +5,11 @@ std::istream& operator>>(std::istream& is, std::back_insert_iterator<std::vector
     T tmp; is >> tmp;  *obj = tmp;   return is;  }
 
 int main() {
+    string a,b,*ap=&a,*bp=&b;
+    debug(ap);
+    debug(bp);
+    debug(ap<b));
+    debug(&a-&b);
     vector<int> v;
     auto it = back_inserter(v);
     while(cin>>it);
