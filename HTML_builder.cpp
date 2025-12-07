8e72375a2a56d61f56dad580b8910fe449de75c9
zxc-steve <63087711+zxc-steve@users.noreply.github.com>
2025-12-07 23:26:11 +0000

    remove constexpr

--- HTML_builder.cpp
+++ HTML_builder.cpp
@@ -1,42 +1,48 @@
 #include "My.h"
 #include <numeric> // For std::accumulate
-template<const char* tagName>
-class htmlTag{
+template <char *tagName>
+class htmlTag
+{
 public:
-    htmlTag(initializer_list<string> li):
-    contents{accumulate(li.begin(), li.end(), string{})} 
-        {}
-    operator string() const {
+    htmlTag(initializer_list<string> li) : contents{accumulate(li.begin(), li.end(), string{})}
+    {
+    }
+    operator string() const
+    {
         return "<" + string(tagName) + map2string() + ">" + contents + "</" + string(tagName) + ">\n";
     }
-    htmlTag& setAttribute(const std::string& key, const std::string& value) {
+    htmlTag &setAttribute(const std::string &key, const std::string &value)
+    {
         attributes[key] = value;
         return *this; // for chaining
     }
-    private:
+
+private:
     string contents;
     map<string, string> attributes;
-    string map2string() const{
+    string map2string() const
+    {
         string result;
-        for(const auto& [key, value] : attributes) {
+        for (const auto &[key, value] : attributes)
+        {
             result += " " + key + "='" + value + "'";
         }
         return result;
-    }   
+    }
 };
-constexpr char htmlH1[] = "h1" ;
-constexpr char htmlDiv[] = "div" ;
-constexpr char htmlSpan[] = "span" ;
-constexpr char htmlBody[] = "body" ;
-constexpr char htmlHtml[] = "html" ;
-constexpr char htmlHead[] = "head" ;
-constexpr char htmlA[] = "a" ;
-constexpr char htmlLi[] = "li" ;
-constexpr char htmlP[] = "p" ;
+char htmlH1[] = "h1";
+char htmlDiv[] = "div";
+char htmlSpan[] = "span";
+char htmlBody[] = "body";
+char htmlHtml[] = "html";
+char htmlHead[] = "head";
+char htmlA[] = "a";
+char htmlLi[] = "li";
+char htmlP[] = "p";
 
 using H1 = htmlTag<htmlH1>;
 using Div = htmlTag<htmlDiv>;
-using Span = htmlTag<htmlSpan>; 
+using Span = htmlTag<htmlSpan>;
 using Body = htmlTag<htmlBody>;
 using Html = htmlTag<htmlHtml>;
 using Head = htmlTag<htmlHead>;
@@ -44,17 +50,20 @@ using A = htmlTag<htmlA>;
 using Li = htmlTag<htmlLi>;
 using P = htmlTag<htmlP>;
 
-int main() {
+int main()
+{
     H1 header{"Welcome to My Website", " - Enjoy your stay!"};
-    Div container{"<p>This is a sample paragraph inside a div.</p>",};
+    Div container{
+        "<p>This is a sample paragraph inside a div.</p>",
+    };
     A a1{"Click here to visit example.com"};
     a1.setAttribute("href", "https://www.example.com");
     Span highlight{"This text is highlighted."};
-    Body pageBody{header, container, highlight,a1};
+    Body pageBody{header, container, highlight, a1};
     Html fullPage{pageBody}; // Just to show multiple contents
     cout << string(fullPage) << endl;
     // test map output
     map<string, string> testMap = {{"key1", "value1"}, {"key2", "value2"}};
     cout << testMap << endl;
     return 0;
-}   
+}
