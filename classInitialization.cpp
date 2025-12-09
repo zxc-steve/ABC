a33a88165fb3f9ae39f19cf9c6b2950d7ea65f99
zxc-steve <63087711+zxc-steve@users.noreply.github.com>
2025-12-09 07:22:48 +0000

    add aggregate initialzation

--- classInitialization.cpp
+++ classInitialization.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-#define debug(x) cout<<#x<<"="<<x<<endl;
+#include "My.h"
 class A{
     public: int i; string s; vector<int> v;
 };
@@ -14,17 +11,20 @@ class C{
     C(int i,string s,vector<int>v):i(i),s{s},v{v}{}
 };
 ostream& operator<<(ostream& os,A& a){
-    cout<<a.i<<','<<a.s<<','<<a.v[0]<<endl;return os;}
+    cout<<a.i<<','<<a.s<<','<<a.v<<endl;return os;}
 ostream& operator<<(ostream& os,B& a){
-    cout<<a.i<<','<<a.s<<','<<a.v[0]<<endl;return os;}
+    cout<<a.i<<','<<a.s<<','<<a.v<<endl;return os;}
 ostream& operator<<(ostream& os,C& a){
-    cout<<a.i<<','<<a.s<<','<<a.v[0]<<endl;return os;}
+    cout<<a.i<<','<<a.s<<','<<a.v<<endl;return os;}
 int main() {
-A a ={1,"qq",{2,3}};
-//B b ={1,"qq",{2,3}};  //fail to compile
+A a ={1,"xy",{2,3}};
+  a={2,"qq",{}};    //default converting constructor and default assignment operator
+//auto a1={2,"qq",{}}; //fail
+  //B b ={1,"qq",{2,3}};  //fail to compile
 B b1 ={2};
+b1.s="--";b1={99};
 C c ={1,"qq",{2,3}};
-//auto d={1,"qq",{2,3}};
+//auto d={1,"qq",{2,3}}; // fail to call constructor of C
 debug(a);debug(b1);debug(c);
     return 0;
 }
